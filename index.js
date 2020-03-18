10f2c17c4c5a79437d34221b18a92a9674851f51
raoenhui <raoenhui@jd.com>
2020-03-18 16:10:45 +0800

    feat: add andorid source

--- index.js
+++ index.js
@@ -5,5 +5,26 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+// 调试预加载的模块 start
+/* const modules = require.getModules();
+const moduleIds = Object.keys(modules);
+const loadedModuleNames = moduleIds
+  .filter(moduleId => modules[moduleId].isInitialized)
+  .map(moduleId => modules[moduleId].verboseName);
+const waitingModuleNames = moduleIds
+  .filter(moduleId => !modules[moduleId].isInitialized)
+  .map(moduleId => modules[moduleId].verboseName);
+
+// make sure that the modules you expect to be waiting are actually waiting
+console.log(
+  'loaded:',
+  loadedModuleNames.length,
+  'waiting:',
+  waitingModuleNames.length
+);
+
+// grab this text blob, and put it in a file named packager/modulePaths.js
+console.log(`module.exports = ${JSON.stringify(loadedModuleNames.sort())};`); */
+// 调试预加载的模块 end
 
 AppRegistry.registerComponent(appName, () => App);
