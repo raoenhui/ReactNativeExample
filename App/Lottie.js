b1d8f1e6b775e8087e4bb0b732cb2f2ca59719b7
raoenhui <raoenhui@jd.com>
2020-03-19 16:07:21 +0800

    lottie init

---
+++ App/Lottie.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text } from 'react-native';
+// import LottieView from 'lottie-react-native';
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            width: 1000,
+            height: 1000
+        }
+    })
+
+export default class Lottie extends Component {
+    render() {
+        // const {sourceJson,isAuto,isLoop,anotherStyle} = this.props;
+        return (
+            <Text>1</Text>
+            // <LottieView
+            //     source={require('./json/test.json')}
+            //     autoPlay={true}
+            //     style={styles.container} />
+  
+        )
+    }
+}
